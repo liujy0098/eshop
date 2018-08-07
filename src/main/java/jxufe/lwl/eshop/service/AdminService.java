e3127ebbe74c6fcac75166166b38b641e6040b8e
jxufewan <15079184013@163.com>
2018-08-07 10:24:27 +0800

    service层

---
+++ src/main/java/jxufe/lwl/eshop/service/AdminService.java
@@ -0,0 +1,7 @@
+package jxufe.lwl.eshop.service;
+
+/**
+ * Created by Administrator on 2018/8/7 0007.
+ */
+public interface AdminService {
+}
---
+++ src/main/java/jxufe/lwl/eshop/service/impl/AdminServiceImpl.java
@@ -0,0 +1,11 @@
+package jxufe.lwl.eshop.service.impl;
+
+import jxufe.lwl.eshop.service.AdminService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Administrator on 2018/8/7 0007.
+ */
+@Service("adminService")
+public class AdminServiceImpl implements AdminService {
+}
