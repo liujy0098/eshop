10c966adb7477e7fa113bb12a9bbf875291ad494
liujy0098 <liujy0098@outlook.com>
2018-08-08 16:59:50 +0800

    ljy

--- src/main/java/jxufe/lwl/eshop/controller/PageController.java
+++ src/main/java/jxufe/lwl/eshop/controller/PageController.java
@@ -6,12 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
-    @RequestMapping("")
-    public String toManager(){
-        return "manager";
-    }
+
+
     @RequestMapping("{module}Manager")
-    public String torticleManager(@PathVariable("module")String module){
+    public String toManager(@PathVariable("module")String module){
         return module+"Manager";
     }
 }
