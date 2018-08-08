2075e671d85741116cf7eea73ffcff0eca9247d1
liujy0098 <liujy0098@outlook.com>
2018-08-08 08:35:53 +0800

    加了主页

--- src/main/java/jxufe/lwl/eshop/controller/PageController.java
+++ src/main/java/jxufe/lwl/eshop/controller/PageController.java
@@ -1,17 +1,17 @@
 package jxufe.lwl.eshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
-    @RequestMapping("index")
-    public String toIndex(){
-        return "index";
-    }
-
-    @RequestMapping("manager")
+    @RequestMapping("")
     public String toManager(){
         return "manager";
+    }
+    @RequestMapping("{module}Manager")
+    public String torticleManager(@PathVariable("module")String module){
+        return module+"Manager";
     }
 }
