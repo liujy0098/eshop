987d6c0ce5cb9f3873d9cb234bb4dd7fdb2cc607
liujy0098 <liujy0098@outlook.com>
2018-08-08 10:03:17 +0800

    ljy

--- src/main/java/jxufe/lwl/eshop/dao/GoodsCommentDAO.java
+++ src/main/java/jxufe/lwl/eshop/dao/GoodsCommentDAO.java
@@ -1,10 +1,9 @@
 package jxufe.lwl.eshop.dao;
 
 import jxufe.lwl.eshop.entity.GoodsComment;
-import jxufe.lwl.eshop.entity.GoodsCommentExample;
 
 /**
  * GoodsCommentDAO继承基类
  */
-public interface GoodsCommentDAO extends MyBatisBaseDao<GoodsComment, Integer, GoodsCommentExample> {
+public interface GoodsCommentDAO extends MyBatisBaseDao<GoodsComment, Integer> {
 }
