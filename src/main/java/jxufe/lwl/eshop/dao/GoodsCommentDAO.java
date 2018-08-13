4e30c9af9f49a14e18a44bef5d8e95a3a30a2965
liujy0098 <liujy0098@outlook.com>
2018-08-13 15:57:08 +0800

    ljy

--- src/main/java/jxufe/lwl/eshop/dao/GoodsCommentDAO.java
+++ src/main/java/jxufe/lwl/eshop/dao/GoodsCommentDAO.java
@@ -2,8 +2,11 @@ package jxufe.lwl.eshop.dao;
 
 import jxufe.lwl.eshop.entity.GoodsComment;
 
+import java.util.List;
+
 /**
  * GoodsCommentDAO继承基类
  */
 public interface GoodsCommentDAO extends MyBatisBaseDao<GoodsComment, Integer> {
+    List<GoodsComment> findAll();
 }
--- src/main/java/jxufe/lwl/eshop/dao/OrderinfoDAO.java
+++ src/main/java/jxufe/lwl/eshop/dao/OrderinfoDAO.java
@@ -1,8 +1,12 @@
 package jxufe.lwl.eshop.dao;
 
 import jxufe.lwl.eshop.entity.Orderinfo;
+
+import java.util.List;
+
 /**
  * OrderinfoDAO继承基类
  */
 public interface OrderinfoDAO extends MyBatisBaseDao<Orderinfo, Integer> {
+    List<Orderinfo> findAll();
 }
