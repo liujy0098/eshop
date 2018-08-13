490f2faa25c864134ebd933ba604efdade9a77a1
jxufewan <15079184013@163.com>
2018-08-13 08:57:46 +0800

    更新

--- src/main/java/jxufe/lwl/eshop/dao/GoodsCategoryDAO.java
+++ src/main/java/jxufe/lwl/eshop/dao/GoodsCategoryDAO.java
@@ -2,9 +2,12 @@ package jxufe.lwl.eshop.dao;
 
 import jxufe.lwl.eshop.entity.GoodsCategory;
 
+import java.util.List;
+
 
 /**
  * GoodsCategoryDAO继承基类
  */
 public interface GoodsCategoryDAO extends MyBatisBaseDao<GoodsCategory, Integer> {
+    List<GoodsCategory> findAll();
 }
