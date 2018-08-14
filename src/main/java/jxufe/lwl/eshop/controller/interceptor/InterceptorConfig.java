7c1c69dcf7112abf5d870555957120fd6c0794bb
liujy0098 <liujy0098@outlook.com>
2018-08-14 19:11:44 +0800

    ljy

---
+++ src/main/java/jxufe/lwl/eshop/controller/interceptor/InterceptorConfig.java
@@ -0,0 +1,48 @@
+package jxufe.lwl.eshop.controller.interceptor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.PrintWriter;
+
+public class InterceptorConfig  implements HandlerInterceptor {
+
+    private static final Logger log = LoggerFactory.getLogger(InterceptorConfig.class);
+
+    /**
+     * 进入controller层之前拦截请求
+     * @param httpServletRequest
+     * @param httpServletResponse
+     * @param o
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+
+        log.info("---------------------开始进入请求地址拦截----------------------------");
+        HttpSession session = httpServletRequest.getSession();
+        if(!StringUtils.isEmpty(session.getAttribute("admin"))){
+            return true;
+        }
+        else{
+            httpServletResponse.sendRedirect("login");
+            return false;
+        }
+
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+    }
+}
